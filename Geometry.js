1350e43ae07b533d51c4100f8159d009c912bede
Vodkacannon <armend.veseli@yahoo.com>
2013-01-06 12:35:51 -0800

    Create Geometry.js

---
+++ Geometry.js
@@ -0,0 +1,13 @@
+//distance formula
+
+function dist_2d ( x, y ) {
+
+  return Math.sqrt ( Math.abs ( x * x ) + Math.abs ( y * y ) )
+
+}
+
+function dist_3d ( x, y, z ) {
+
+  return Math.sqrt ( Math.abs ( x * x ) + Math.abs ( y * y ) + Math.abs (z * z) )
+
+}
